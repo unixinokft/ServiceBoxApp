f709432cba9d78efd6391f346984e8e81fa16b97
Manage0 <jkbbenedek@gmail.com>
2025-01-02 18:48:38 +0100

    removed reset-project

--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -1,11 +1,15 @@
-import 'react-native-url-polyfill/auto';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
 
+//Move these to .env
+//For eas, add them to the builds and not in the eas.json directly
 const supabaseUrl = "https://xyztxxvjjwbtwvwrzgtx.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5enR4eHZqandidHd2d3J6Z3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0NTU4MDgsImV4cCI6MjA0NjAzMTgwOH0.YpaZzGfFQ-vFGox0Kx0nA1vragmd8pMbuV_66TIPTp8";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5enR4eHZqandidHd2d3J6Z3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0NTU4MDgsImV4cCI6MjA0NjAzMTgwOH0.YpaZzGfFQ-vFGox0Kx0nA1vragmd8pMbuV_66TIPTp8";
 // const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5enR4eHZqandidHd2d3J6Z3R4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDQ1NTgwOCwiZXhwIjoyMDQ2MDMxODA4fQ.eX8-bkRZodl3R89gDF7gfDkxi5sr0dNH8eBsM5yWbas";
 
+//This worth a review, mostly for the remember me feature, but other undiscovered functionalities can be found
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
@@ -15,4 +19,4 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   },
 });
 
-export default supabase
+export default supabase;
